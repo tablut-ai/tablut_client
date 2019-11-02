8a3f5db3a0b70cb78d211f4d89f0c41d024fc3c0
DanieleVeri <daniele.veri.96@gmail.com>
2019-11-02 20:49:51 +0100

    added aima search libs

--- src/client.py
+++ src/client.py
@@ -1,6 +1,8 @@
 import socket
 import json
 import sys
+import math
+from aima.search import Problem, Node, breadth_first_graph_search
 
 if len(sys.argv) != 3:
     print("Usage: client server_host server_port")
@@ -11,6 +13,15 @@ server_address = (sys.argv[1], int(sys.argv[2]))
 sock.connect(server_address)
 
 try:
+    # sending name
+    sock.sendall(b'Ragnarok')
+
+    #data = sock.recv(64)
+    #print(data)
+
+    while True:
+        time.sleep(0.2)
+
     message = {
         "x": "A",
         "y": 5,
