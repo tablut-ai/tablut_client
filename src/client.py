6a8ce9b9d4c9b173d91b6f49ce8d0df4590966b7
DanieleVeri <daniele.veri.96@gmail.com>
2019-11-04 10:03:29 +0100

    added base game loop

--- src/client.py
+++ src/client.py
@@ -2,33 +2,64 @@ import socket
 import json
 import sys
 import math
+import time
 from aima.search import Problem, Node, breadth_first_graph_search
 
+def usage():
+    print("Usage: client server_host <white | black>")
+
 if len(sys.argv) != 3:
-    print("Usage: client server_host server_port")
+    usage()
     exit(1)
 
+color=sys.argv[2]
+if color == "white":
+    port = 5800
+elif color == "black":
+    port = 5801
+else:
+    usage()
+    exit(1)
+
+def send_utf(sock, string):
+    encoded = string.encode("UTF-8")
+    length = len(encoded).to_bytes(4, 'big')
+    sock.sendall(length+encoded)
+
+def recv_json(sock):
+    char = sock.recv(1)
+    while(char == b'\x00'):
+        char = sock.recv(1)
+    length_str = char + sock.recv(1)
+    total = int.from_bytes(length_str, "big")
+    msg = sock.recv(total)
+    deserialized = json.loads(msg.decode("UTF-8"))
+    return deserialized
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_address = (sys.argv[1], int(sys.argv[2]))
+server_address = (sys.argv[1], port)
 sock.connect(server_address)
 
 try:
-    # sending name
-    sock.sendall(b'Ragnarok')
+    # present name
+    send_utf(sock, "Ragnarok")
 
-    #data = sock.recv(64)
-    #print(data)
+    ####################################### game loop
+    # wait init state
+    print(recv_json(sock))
+
+    if color=="white":
+        message = {"from": "d5", "to": "d6"}
+    if color=="black":
+        data = sock.recv(2)
+        print(data)
+        message = {"from": "a4", "to": "b4"}
+
+    send_utf(sock, json.dumps(message))
 
     while True:
+        print(recv_json(sock))
         time.sleep(0.2)
-
-    message = {
-        "x": "A",
-        "y": 5,
-        "direction": 1,
-        "amount": 4
-    }
-    sock.sendall(json.dumps(message).encode('utf-8'))
 
 finally:
     print('closing socket')
