faca5d71040aae683934717dad2e4de13044e28f
EleMisi <33552669+EleMisi@users.noreply.github.com>
2019-11-05 11:42:04 +0100

    Update game.py
    Penso che tutte le funzioni debbano essere inserite nella classe Tablut (che assumerà il ruolo assunto dalla classe Game di aima)

--- src/game.py
+++ src/game.py
@@ -1,11 +1,13 @@
 from aima.search import Problem, Node, breadth_first_graph_search
 
-def next_move(state):
-    if state["turn"] == "WHITE":            
-        move = {"from": "d5", "to": "d6"}
-    if state["turn"] == "BLACK":           
-        move = {"from": "a4", "to": "b4"}
-    return move
+class Tablut:
+
+    def next_move(state):
+        if state["turn"] == "WHITE":            
+            move = {"from": "d5", "to": "d6"}
+        if state["turn"] == "BLACK":           
+            move = {"from": "a4", "to": "b4"}
+        return move
     
-def is_valid_move(state, move):
-    return None
+    def is_valid_move(state, move):
+        pass
