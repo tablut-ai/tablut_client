bc472b82f4f824ec202296235c6e11c749600c4c
BogdanIvasiuk <56299600+BogdanIvasiuk@users.noreply.github.com>
2019-11-06 18:35:04 +0100

    Add files via upload

---
+++ src/BogCode/45D1S.py
@@ -0,0 +1,165 @@
+###############################################################################
+# Import Library
+###############################################################################
+from collections import OrderedDict, namedtuple
+
+###############################################################################
+# Global constants
+###############################################################################
+
+#Board
+# Our board is represented as a X character string. How many character?
+
+# Lists of possible moves for each piece type.
+
+
+
+MATE_LOWER = 0
+MATE_UPPER = 0
+
+
+# The table size is the maximum number of elements in the transposition table.
+TABLE_SIZE = 1e8
+
+
+# Constants for tuning search
+QSearch_LIMIT = 150
+EVAL_ROUGHNESS = 20
+
+
+###############################################################################
+# Tablut logic
+###############################################################################
+
+
+class Position():
+
+    def move(self,move):
+
+    return Position()
+
+
+
+###############################################################################
+# Search logic
+###############################################################################
+
+# lower <= s(pos) <= upper
+Entry = namedtuple('Entry', 'lower upper')
+
+
+class LRUCache:
+    '''Store items in the order the keys were last added'''
+    def __init__(self, size):
+        self.od = OrderedDict()
+        self.size = size
+
+    def get(self, key, default=None):
+        try: self.od.move_to_end(key)
+        except KeyError: return default
+        return self.od[key]
+
+    def __setitem__(self, key, value):
+        try: del self.od[key]
+        except KeyError:
+            if len(self.od) == self.size:
+                self.od.popitem(last=False)
+        self.od[key] = value
+
+class Searcher:
+    def __init__(self):
+        self.tp_score = LRUCache(TABLE_SIZE)
+        self.tp_move = LRUCache(TABLE_SIZE)
+        self.nodes = 0
+
+    def bound(self, pos, gamma, depth, root=True):
+        """ returns r where
+                s(pos) <= r < gamma    if gamma > s(pos)
+                gamma <= r <= s(pos)   if gamma <= s(pos)"""
+        self.nodes += 1
+
+
+        depth = max(depth, 0)
+
+
+        if pos.score <= -MATE_LOWER:
+            return -MATE_UPPER
+
+
+        entry = self.tp_score.get((pos, depth, root), Entry(-MATE_UPPER, MATE_UPPER))
+        if entry.lower >= gamma and (not root or self.tp_move.get(pos) is not None):
+            return entry.lower
+        if entry.upper < gamma:
+            return entry.upper
+
+
+        def moves():
+            # First try not moving at all
+            if depth > 0 and not root and any(c in pos.board for c in 'RBNQ'):
+                yield None, -self.bound(pos.nullmove(), 1-gamma, depth-3, root=False)
+            # For QSearch we have a different kind of null-move
+            if depth == 0:
+                yield None, pos.score
+
+            killer = self.tp_move.get(pos)
+            if killer and (depth > 0 or pos.value(killer) >= QSearch_LIMIT):
+                yield killer, -self.bound(pos.move(killer), 1-gamma, depth-1, root=False)
+            # Then all the other moves
+            for move in sorted(pos.gen_moves(), key=pos.value, reverse=True):
+                if depth > 0 or pos.value(move) >= QSearch_LIMIT:
+                    yield move, -self.bound(pos.move(move), 1-gamma, depth-1, root=False)
+
+        # Run through the moves, shortcutting when possible
+        best = -MATE_UPPER
+        for move, score in moves():
+            best = max(best, score)
+            if best >= gamma:
+                # Save the move for pv construction and killer heuristic
+                self.tp_move[pos] = move
+                break
+
+
+        if best < gamma and best < 0 and depth > 0:
+            is_dead = lambda pos: any(pos.value(m) >= MATE_LOWER for m in pos.gen_moves())
+            if all(is_dead(pos.move(m)) for m in pos.gen_moves()):
+                in_check = is_dead(pos.nullmove())
+                best = -MATE_UPPER if in_check else 0
+
+        # Table part 2
+        if best >= gamma:
+            self.tp_score[(pos, depth, root)] = Entry(best, entry.upper)
+        if best < gamma:
+            self.tp_score[(pos, depth, root)] = Entry(entry.lower, best)
+
+        return best
+
+
+    def _search(self, pos):
+        """ Iterative deepening MTD-bi search """
+        self.nodes = 0
+
+        #
+        for depth in range(1, 1000):
+            self.depth = depth
+
+            lower, upper = -MATE_UPPER, MATE_UPPER
+            while lower < upper - EVAL_ROUGHNESS:
+                gamma = (lower+upper+1)//2
+                score = self.bound(pos, gamma, depth)
+                if score >= gamma:
+                    lower = score
+                if score < gamma:
+                    upper = score
+
+            score = self.bound(pos, lower, depth)
+
+
+
+    def search(self, pos, secs):
+        start = time.time()
+        for _ in self._search(pos):
+            if time.time() - start > secs:
+                break
+
+        return self.tp_move.get(pos), self.tp_score.get((pos, self.depth, True)).lower
+
